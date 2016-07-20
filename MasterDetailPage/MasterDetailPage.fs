4ce664870cec043acc82d9153a0b4e8232cad943
Sam Williams <willsam100@gmail.com>
2016-07-21 07:31:43 +1200

    Project setup

---
+++ MasterDetailPage/MasterDetailPage.fs
@@ -0,0 +1,35 @@
+﻿namespace MasterDetailPage
+
+open System
+open Xamarin.Forms
+open Xamarin.Forms.Xaml
+
+type MasterPageItem = {
+    Title:string 
+    IconSource: string
+}
+
+//type ContactsPage() =
+//    inherit ContentPage()
+     
+    //do base.Content <- Label(Text = "Hello")
+
+
+type MasterPage() =
+    inherit ContentPage() 
+
+    let _ = base.LoadFromXaml(typeof<MasterPage>)
+
+    let listView = base.FindByName<ListView>("listView")
+    let masterPageItems = [{Title = "Contacts"; IconSource = "contacts.png"}]
+    do listView.ItemsSource = ((masterPageItems |> List.toSeq) :> Collections.IEnumerable) 
+        |> ignore
+
+type MainPage() =
+    inherit MasterDetailPage()
+    let _ = base.LoadFromXaml(typeof<MainPage>)
+
+
+type App() = 
+    inherit Application(MainPage = MainPage())
+
